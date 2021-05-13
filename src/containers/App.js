9ed973daf0d3a86942a8562c602be2779a8dd32f
Katherine Nightingale <57119972+katnightingale@users.noreply.github.com>
2021-05-13 14:06:27 -0400

    Fixed some security issues and updated libraries; cleaned up folder structure.

---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Scroll from '../components/Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			robots: [],
+			searchField: ''
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => {this.setState({robots: users})});
+		
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchField: event.target.value})
+		
+		
+	}
+
+	render() {
+		const {robots, searchField} = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
+		})
+		return !robots.length ?
+			<h1>Loading...</h1> :
+			 (
+				<div className="tc">
+					<h1 className="f1">RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<Scroll>
+						<CardList robots={filteredRobots}/>
+					</Scroll>
+				</div>
+			)
+	}
+}
+
+export default App;
